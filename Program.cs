0af107df31a18b5c6385ecaff111265724ba18fc
m-hanafi <mohdhanafimohtar@gmail.com>
2022-11-08 18:23:27 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace Palindrom_Level
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text:");
+            string x = Console.ReadLine();
+            int res = Palindrom(x);
+            Console.WriteLine($"Palindrom level:{res}");
+        }
+
+        static int lvl = 0;
+        static int Palindrom(string x)
+        {
+            x = x.Replace(" ",""); //remove space
+            x = x.ToLower(); //convert text to lowercase
+
+            //reverse the text by re-order the charater from last index
+            string rev=string.Empty;
+            for (int i=x.Length-1;i>=0;i--)
+            {
+                rev +=x[i];
+            }
+
+            //check if palindrom
+            if (x == rev)
+            {
+                lvl++; //increase palindrom level by 1
+                int x_len;
+                
+                if(x.Length%2!=0) 
+                {
+                    //take character in the middle if length is odd number
+                   x_len = x.Length / 2 + 1;
+
+                }
+                else{
+                    x_len = x.Length / 2;
+                }
+
+                x = x.Substring(0, x_len); //split the text to get another palindrom level
+                Palindrom(x); //recursive function
+            }
+
+            return lvl;
+
+        }
+    }
+}
